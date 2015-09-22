6a08f78f861f386bbedf5262bb1c0a7270434f6c
dotnext24 <dotnext24@gmail.com>
2015-09-22 16:04:07 +0530

    Initial Commit

---
+++ SignalIR/SignalIR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalIR.Startup))]
+namespace SignalIR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
